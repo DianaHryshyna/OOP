79839a33e40b288fd6853bbb7f770144dba7f88e
Diana <32524600+DianaHryshyna@users.noreply.github.com>
2018-02-22 21:54:18 +0300

    .

--- 22.02.18/7.2.1/7.2.1/maincpp.cpp
+++ 22.02.18/7.2.1/7.2.1/maincpp.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
 using namespace std;
+
 struct Triangle {
 	int a;
 	int b;
 	int c;
 };
 
-int Perimeter(Triangle tria){
+int perimeter(Triangle tria){
 	return tria.a + tria.b + tria.c;
 }
+
 int main() {
 	Triangle parametry = { 2, 2, 1 };
-	int result = Perimeter(parametry);
+	int result = perimeter(parametry);
 	cout << "The perimeter is " << result << "." << endl;
 	system("pause");
 	return 0;
