003fef3f8cb2f688d9e3f7aa2d6ed974bbc89948
Diana <32524600+DianaHryshyna@users.noreply.github.com>
2018-02-22 22:17:51 +0300

    .

--- 22.02.18/7.2.2/7.2.2/main.cpp
+++ 22.02.18/7.2.2/7.2.2/main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+struct Triangle {
+	int a;
+	int b;
+	int c;
+};
+
+void assign(Triangle troj1, Triangle* troj2) {
+	*troj2 = troj1;
+}
+
 int main() {
+	Triangle parametry = { 1, 2, 3 };
+	Triangle parametry1 = { 2, 3, 4 };
+	assign(parametry, &parametry1);
+	cout << parametry1.a << ends;
+	cout << parametry1.b << ends;
+	cout << parametry1.c << ends;
 	system("pause");
 	return 0;
 }
