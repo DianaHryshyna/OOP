4bb6429fdff0754374ce2746e9e9de594dd68570
Diana <32524600+DianaHryshyna@users.noreply.github.com>
2018-04-01 00:12:31 +0300

    .

---
+++ 22.03.18/zad1/zad1/Villager.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Villager
+{
+public:
+	Villager(string name, unsigned int age, unsigned int m_age, bool sex);
+	Villager();
+	Villager(Villager& villager);
+	void advance();
+	string getName();
+	~Villager();
+private:
+	string* name;
+	string m_name[3] = { "Adrian", "Paweł", "Józef" };
+	string f_name[3] = { "Krystyna", "Ola", "Ania" };
+	unsigned int age;
+	unsigned int m_age;
+	bool sex; //true - M, false - F
+
+};
+
